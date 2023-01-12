f0f62cf4f9a2b0fd46a4242f94fc2c3ff298509f
dnipy <dnipy@protonmail.com>
2023-01-12 13:51:12 +0000

    scripts dir and pm2_runner.sh - test four

--- scripts/pm2_runner.sh
+++ scripts/pm2_runner.sh
@@ -3,4 +3,4 @@ then
     sudo npm install -g pm2 && pm2 delete expressjs && pm2 start npm --name expressjs -i 2 -- run start -- -p 3001 
 else
     pm2 delete expressjs && pm2 start npm --name expressjs -i 2 -- run start -- -p 3001 
-fi  
+fi    
