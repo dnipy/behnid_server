c87b3e4eaea23d4eb5e137615b3cf2591647c8ba
dnipy <dnipy@protonmail.com>
2023-02-18 23:36:25 +0330

    midlleware changes

--- middlewares/authorizeMiddleware.middlewares.js
+++ middlewares/authorizeMiddleware.middlewares.js
@@ -8,7 +8,7 @@ function authorizeMiddleware(req, res, next) {
     const authHeader = req.headers["authorization"]
     const token = authHeader ? authHeader.split(" ")[1] : null
 
-    if (token === null) return res.json({ err: "یوزری با این مشخصات پیدا نشد" })
+    if (token === null) return res.json({ err: "حسابی با این مشخصات پیدا نشد" })
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SEC, (err, userData) => {
         if (err) return res.json({ err: "نشست غیر فعال" })
