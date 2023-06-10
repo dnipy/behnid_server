6449b4757acd29d976e3be154f0a068cf0e10b1e
dnipy <dnipy@protonmail.com>
2023-06-10 06:00:34 +0000

    dnipy: fix cors origin issuue

--- middlewares/customCORS.js
+++ middlewares/customCORS.js
@@ -1,5 +1,9 @@
-export const customCors = (_, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://behnid.com');
+export const customCors = (req, res, next) => {
+    let allowedOrigins = ["https://www.behnid.com", "https://behnid.com"]
+    let origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
